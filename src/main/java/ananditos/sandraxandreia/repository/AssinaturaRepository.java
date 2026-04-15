292488e02e2ee2c3edda2eb56a4a8492d9b41685
Guga Abreu <gustavosabreu16@gmail.com>
2026-04-15 08:32:07 -0300

    Assinatura Repository

---
+++ src/main/java/ananditos/sandraxandreia/repository/AssinaturaRepository.java
@@ -0,0 +1,7 @@
+package ananditos.sandraxandreia.repository;
+
+import ananditos.sandraxandreia.domain.Assinatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssinaturaRepository extends JpaRepository<Assinatura, Long> {
+}
