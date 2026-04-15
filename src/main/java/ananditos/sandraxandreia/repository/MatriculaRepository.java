d5135a72eded9d9c8020142218e15f0d895e9c01
Guga Abreu <gustavosabreu16@gmail.com>
2026-04-15 08:43:57 -0300

    Matricula Repository

---
+++ src/main/java/ananditos/sandraxandreia/repository/MatriculaRepository.java
@@ -0,0 +1,7 @@
+package ananditos.sandraxandreia.repository;
+
+import ananditos.sandraxandreia.domain.Matricula;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatriculaRepository extends JpaRepository<Matricula, Long> {
+}
