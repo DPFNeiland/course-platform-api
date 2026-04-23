b6502ee7e1c5f404134035b48212b42118aa60b1
Neiland <rodrigo.naohan@gmail.com>
2026-04-23 11:11:30 -0300

    Update UsuarioService.java

--- src/main/java/ananditos/sandraxandreia/service/UsuarioService.java
+++ src/main/java/ananditos/sandraxandreia/service/UsuarioService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class x'UsuarioService {
+public class UsuarioService {
 
     private final UsuarioRepository repository;
     private final PasswordEncoder passwordEncoder;
